58ccc7dbf5f7b7c6bd2fabf9baa50b37376bb2b1
schrodobaggins <magicalmikely@gmail.com>
2021-12-09 20:37:45 -0800

    pushing tests

---
+++ FastingAppTests/FastingAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  FastingAppTests.swift
+//  16ToGo Fasting Countdown
+//
+//  Created by Michael Schroeder
+//
+
+import XCTest
+@testable import FastingApp
+
+class FastingAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
